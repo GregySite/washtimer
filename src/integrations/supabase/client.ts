17af26e268f316b5d41825b826680e7821134884
Greg <101050106+GregySite@users.noreply.github.com>
2026-01-19 22:37:48 +0200

    Correction nom package supabase

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-client';
+import { createClient } from '@supabase/supabase-js';
 
 // Remplace 'TA_CLE_ANON_ICI' par la longue suite de caractères que tu as copiée
 const supabaseUrl = 'https://sojsinqouukjmikuwobi.supabase.co';
